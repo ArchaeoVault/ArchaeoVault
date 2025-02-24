45bf8effe80ff314c9ab8be678d4ca0e38f277c1
Rory Paul <prory101@gmail.com>
2025-02-24 14:25:46 -0500

    Split settings.py file into base, developer, and production settings

---
+++ app/app/production.py
@@ -0,0 +1,23 @@
+# production.py
+import os
+from .base import *
+DEBUG = False
+
+ALLOWED_HOSTS = [os.environ.get('DJANGO_ALLOWED_HOST_1', 'None'), os.environ.get('DJANGO_ALLOWED_HOST_2', 'None')]
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'None')
+
+DATABASES = {
+    
+    'default': {
+        'ENGINE': os.environ.get('ARCHAEODB_ENGINE', 'None'),
+        'NAME': os.environ.get('ARCHAEODB_NAME', 'None'),
+        'USER': os.environ.get('ARCHAEODB_USER', 'None'),
+        'PASSWORD': os.environ.get('ARCHAEODB_PASSWORD', 'None'),
+        'HOST': os.environ.get('ARCHAEODB_HOST', 'None'),
+        'PORT': os.environ.get('ARCHAEODB_PORT', 'None'),
+    }
+}
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/static/'

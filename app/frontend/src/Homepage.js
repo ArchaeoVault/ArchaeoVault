c5d743cbee50f8e30775ed04c4597299886a76f2
Jaylen Morris <jaylenmorris@gmail.com>
2025-02-24 14:04:03 -0500

    added placeholder image

--- app/frontend/src/Homepage.js
+++ app/frontend/src/Homepage.js
@@ -21,7 +21,7 @@ const HomePage = () => {
       <section className="hero">
         <div className="hero-image">
           <img
-            src="https://via.placeholder.com/600x600"
+            src="/placeholder.jpg"
             alt="Artifact Display"
           />
         </div>

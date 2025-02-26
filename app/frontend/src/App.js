c7be934c072d38976dc93fddf7f66606e338d98c
alvingeorge567 <albingeorge567@gmail.com>
2025-02-26 15:31:25 -0500

    Save changes to App.js before pulling

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./Homepage";
 import Login from "./Login";
 import ForgotPassword from "./ForgotPassword";

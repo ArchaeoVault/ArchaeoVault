e3efdc8559a031aac96ada0f1fabba320f06b266
alvingeorge567 <albingeorge567@gmail.com>
2025-03-19 12:19:41 -0400

    redid the forgot/reset password functionality again

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Homepage";
 import Login from "./Login";
-import ForgotPassword from "./ForgotPassword";
 import Signup from './Signup';
+import ForgotPassword from "./ForgotPassword";
+import ResetPassword from "./ResetPassword";
 import Artifacts from './Artifacts';
 import NewportArtifacts from './NewportArtifacts';
 import PortsmouthArtifacts from './PortsmouthArtifacts';
@@ -18,6 +19,7 @@ const App = () => {
        <Route path="/artifacts" element={<Artifacts />} />
        <Route path="/signup" element={<Signup />} />
        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset/:uidb64/:token" element={<ResetPassword />} />
        <Route path="/newport-artifacts" element={<NewportArtifacts />} />
        <Route path="/portsmouth-artifacts" element={<PortsmouthArtifacts />} />
        <Route path="/" element={showLogin ? <Login /> : <HomePage setShowLogin={setShowLogin} />} />

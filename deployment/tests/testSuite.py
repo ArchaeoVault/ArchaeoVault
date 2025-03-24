24260ddae0bade1b5594e9dfa47e9afe161b3d4d
jhollenbach21 <163784396+jhollenbach21@users.noreply.github.com>
2025-03-24 13:40:54 -0400

    Importing other unit tests into testSuite

---
+++ deployment/tests/testSuite.py
@@ -0,0 +1,18 @@
+import unittest
+import testSetup
+import testTearDown
+import os
+import sys
+
+sys.path.append("../../app/myapp")
+import test_create_user
+sys.path.append("../../deployment/tests")
+
+
+
+print(os.getcwd())
+
+print("thos")
+
+#if __name__ == '__main__':
+    #unittest.main()

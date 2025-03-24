36dcebdab4f5337c01d50d9a728c7ab2616fa452
Rory Paul <prory101@gmail.com>
2025-03-24 19:51:57 -0400

    Moving react deployment step to deployment script

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -9,6 +9,16 @@ cd app
 chmod +x manage.py
 ./manage.py makemigrations
 ./manage.py migrate  --run-syncdb
+
+cd /var/www/html/ArchaeoVault/app/frontend
+npm run build
+cd /var/www/html
+mv /var/www/html/ArchaeoVault/app/frontend/build/* /var/www/html/production/
+sudo chown -R www-data:www-data /var/www/html/production
+sudo chmod -R 755 /var/www/html/production
+sudo service nginx restart
+
+
 sudo service gunicorn restart
 sudo service nginx restart
 exit

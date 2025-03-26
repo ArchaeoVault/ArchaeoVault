94fa4bd0620b9b66ce44f0678e78cb07f48d38d9
jhollenbach21 <163784396+jhollenbach21@users.noreply.github.com>
2025-03-26 13:53:13 -0400

    TestSuite now can grab other test files and run those tests, test_UATs now runs UAT tests on user story #19. Selenium added to requirements.txt

--- deployment/tests/testSuite.py
+++ deployment/tests/testSuite.py
@@ -1,16 +1,22 @@
 import unittest
-import testSetup
-import testTearDown
-import os
 import sys
+
+
 
 sys.path.append("myapp")
 import test_create_user
-sys.path.append("../../deployment/tests")
+sys.path.append("../../deployment/tests/UAT_tests")
+import test_UATs.test_UAT_userstory19
+
+# Get the current working directory
+
+#python3 manage.py test ../deployment/tests
 
 def load_tests(loader, standard_tests, pattern):
     test_suite = unittest.TestSuite()
     test_suite.addTests(loader.loadTestsFromModule(test_create_user))
+    test_suite.addTests(loader.loadTestsFromModule(test_UATs.test_UAT_userstory19))
+
     return test_suite
 
 if __name__ == '__main__':
---
+++ deployment/tests/test_UATs/test_UAT_userstory19.py
@@ -0,0 +1,162 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+
+class test_UAT_userstory19(unittest.TestCase):
+	def setUp(self):
+		self.driver = webdriver.Chrome()
+		self.driver.get("https://archaeovault.com")
+		login_page_button = self.driver.find_element(by = By.LINK_TEXT, value = "Login")
+		login_page_button.click()
+
+	def test_valid_email_with_valid_password(self):
+		self.driver.implicitly_wait(1)
+		emailBox = self.driver.find_element(by = By.XPATH, value = "//input[@placeholder='Email']")
+		passwordBox = self.driver.find_element(by = By.XPATH, value = "//input[@placeholder='Password']")
+		emailBox.send_keys("temp@email.com")
+		passwordBox.send_keys("password123")
+		submitButton = self.driver.find_element(By.XPATH, "//button[text()='Log In']")
+		submitButton.click()
+		try:
+			WebDriverWait(self.driver, 3).until(EC.alert_is_present())
+			alert = self.driver.switch_to.alert
+			message = alert.text
+			alert.accept()
+		except TimeoutException:
+			assert False
+		assert message == "Login successful!"
+
+	def test_valid_email_with_invalid_password(self):
+		self.driver.implicitly_wait(1)
+		emailBox = self.driver.find_element(by = By.XPATH, value = "//input[@placeholder='Email']")
+		passwordBox = self.driver.find_element(by = By.XPATH, value = "//input[@placeholder='Password']")
+		emailBox.send_keys("temp@email.com")
+		passwordBox.send_keys("wrongpassword")
+		submitButton = self.driver.find_element(By.XPATH, "//button[text()='Log In']")
+		submitButton.click()
+		try:
+			WebDriverWait(self.driver, 3).until(EC.alert_is_present())
+			alert = self.driver.switch_to.alert
+			message = alert.text
+			alert.accept()
+		except TimeoutException:
+			assert False
+		assert message == "Login successful!"
+
+	def test_long_password(self):
+		self.driver.implicitly_wait(1)
+		emailBox = self.driver.find_element(by = By.XPATH, value = "//input[@placeholder='Email']")
+		passwordBox = self.driver.find_element(by = By.XPATH, value = "//input[@placeholder='Password']")
+		emailBox.send_keys("temp@email.com")
+		passwordBox.send_keys("wrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpasswordwrongpassword")
+		submitButton = self.driver.find_element(By.XPATH, "//button[text()='Log In']")
+		submitButton.click()
+		try:
+			WebDriverWait(self.driver, 3).until(EC.alert_is_present())
+			alert = self.driver.switch_to.alert
+			message = alert.text
+			alert.accept()
+		except TimeoutException:
+			assert False
+		assert message == "Login successful!"
+
+	def test_long_email(self):
+		self.driver.implicitly_wait(1)
+		emailBox = self.driver.find_element(by = By.XPATH, value = "//input[@placeholder='Email']")
+		passwordBox = self.driver.find_element(by = By.XPATH, value = "//input[@placeholder='Password']")
+		emailBox.send_keys("temptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemptemp@email.com")
+		passwordBox.send_keys("password123")
+		submitButton = self.driver.find_element(By.XPATH, "//button[text()='Log In']")
+		submitButton.click()
+		try:
+			WebDriverWait(self.driver, 3).until(EC.alert_is_present())
+			alert = self.driver.switch_to.alert
+			message = alert.text
+			alert.accept()
+		except TimeoutException:
+			assert False
+		assert message == "Login successful!"
+	
+	def test_no_email_no_password(self):
+		self.driver.implicitly_wait(1)
+		emailBox = self.driver.find_element(by = By.XPATH, value = "//input[@placeholder='Email']")
+		passwordBox = self.driver.find_element(by = By.XPATH, value = "//input[@placeholder='Password']")
+		emailBox.click()
+		submitButton = self.driver.find_element(By.XPATH, "//button[text()='Log In']")
+		try:
+			validation_message = emailBox.get_attribute("validationMessage")
+		except:
+			print("No validation message found.")
+		assert validation_message == "Please fill out this field."
+
+
+	def test_valid_email_no_password(self):
+		self.driver.implicitly_wait(1)
+		emailBox = self.driver.find_element(by = By.XPATH, value = "//input[@placeholder='Email']")
+		passwordBox = self.driver.find_element(by = By.XPATH, value = "//input[@placeholder='Password']")
+		emailBox.send_keys("temp@email.com")
+		submitButton = self.driver.find_element(By.XPATH, "//button[text()='Log In']")
+		try:
+			validation_message = passwordBox.get_attribute("validationMessage")
+		except:
+			print("No validation message found.")
+		assert validation_message == "Please fill out this field."
+
+	def test_no_email_valid_password(self):
+		self.driver.implicitly_wait(1)
+		emailBox = self.driver.find_element(by = By.XPATH, value = "//input[@placeholder='Email']")
+		passwordBox = self.driver.find_element(by = By.XPATH, value = "//input[@placeholder='Password']")
+		passwordBox.send_keys("password")
+		submitButton = self.driver.find_element(By.XPATH, "//button[text()='Log In']")
+		try:
+			validation_message = emailBox.get_attribute("validationMessage")
+		except:
+			print("No validation message found.")
+		assert validation_message == "Please fill out this field."
+
+	def test_email_sql_injection(self):
+		self.driver.implicitly_wait(1)
+		emailBox = self.driver.find_element(by = By.XPATH, value = "//input[@placeholder='Email']")
+		passwordBox = self.driver.find_element(by = By.XPATH, value = "//input[@placeholder='Password']")
+		emailBox.send_keys("temp@email.com = 1' or '1' = '1")
+		passwordBox.send_keys("password123")
+		submitButton = self.driver.find_element(By.XPATH, "//button[text()='Log In']")
+		submitButton.click()
+		try:
+			validation_message = emailBox.get_attribute("validationMessage")
+		except:
+			print("No validation message found.")
+		assert validation_message == "A part following '@' should not contain the symbol ' '."
+
+	def test_password_sql_injection(self):
+		self.driver.implicitly_wait(1)
+		emailBox = self.driver.find_element(by = By.XPATH, value = "//input[@placeholder='Email']")
+		passwordBox = self.driver.find_element(by = By.XPATH, value = "//input[@placeholder='Password']")
+		emailBox.send_keys("temp@email.com")
+		passwordBox.send_keys("password123 = 1' or '1' = '1")
+		submitButton = self.driver.find_element(By.XPATH, "//button[text()='Log In']")
+		submitButton.click()
+		try:
+			WebDriverWait(self.driver, 3).until(EC.alert_is_present())
+			alert = self.driver.switch_to.alert
+			message = alert.text
+			alert.accept()
+		except TimeoutException:
+			assert False
+		assert message == "Login successful!"
+		
+	def test_forgot_password_button(self):
+		self.driver.implicitly_wait(1)
+		forgotPasswordButton = self.driver.find_element(by = By.LINK_TEXT, value = "Forgot Password?")
+		forgotPasswordButton.click()
+		assert True
+
+	def tearDown(self):
+		self.driver.close()
+
+if __name__ == "__main__":
+	unittest.main()

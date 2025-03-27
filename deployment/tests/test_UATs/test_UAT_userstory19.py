e0f111c626ffeb98ff2256d631cc08bafd9badd1
jhollenbach21 <163784396+jhollenbach21@users.noreply.github.com>
2025-03-26 21:27:40 -0400

    Removed unecessary import from test_suite and created safer way to load into login page

--- deployment/tests/test_UATs/test_UAT_userstory19.py
+++ deployment/tests/test_UATs/test_UAT_userstory19.py
@@ -10,7 +10,7 @@ class test_UAT_userstory19(unittest.TestCase):
 	def setUp(self):
 		self.driver = webdriver.Chrome()
 		self.driver.get("https://archaeovault.com")
-		login_page_button = self.driver.find_element(by = By.LINK_TEXT, value = "Login")
+		login_page_button = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Login")))
 		login_page_button.click()
 
 	def test_valid_email_with_valid_password(self):

5becc1c95eeb2248b649692c4f7312187fa99dcf
timothyraab <timothyraab21@gmail.com>
2025-04-02 14:08:42 -0400

    Further fixing of model to line up with database schema

--- app/myapp/models.py
+++ app/myapp/models.py
@@ -35,7 +35,7 @@ class users(models.Model):
 
 class organicinorganic(models.Model):
     id = models.IntegerField(primary_key=True)
-    type = models.CharField(max_length=10)
+    type = models.CharField(max_length=20)
 
 class speciestype(models.Model):
     id = models.IntegerField(primary_key=True)
@@ -59,26 +59,26 @@ class conservationtype(models.Model):
 
 class your_table(models.Model):
     address = models.ForeignKey(address, on_delete=models.CASCADE) #foriegn key
-    owner = models.CharField(max_length=9000)
+    owner = models.CharField(max_length=255)
     date_collected = models.DateTimeField()
-    catalog_number = models.CharField(max_length=900, primary_key=True)
-    object_name = models.CharField(max_length=9000)
+    catalog_number = models.CharField(max_length=25, primary_key=True)
+    object_name = models.CharField(max_length=255)
     scanned_3d = models.ForeignKey(threedscannedtable, on_delete=models.CASCADE)  #foriegn key
     printed_3d = models.ForeignKey(threedprintedtable, on_delete=models.CASCADE)  #foriegn key
-    scanned_by = models.CharField(max_length=9000)
+    scanned_by = models.CharField(max_length=100)
     date_excavated = models.DateTimeField()
-    object_dated_to = models.CharField(max_length=9000) #foriegn key
-    object_description = models.CharField(max_length=750)
+    object_dated_to = models.CharField(max_length=100) #foriegn key
+    object_description = models.CharField(max_length=1000)
     organic_inorganic = models.ForeignKey(organicinorganic, on_delete=models.CASCADE) #foriegn key
     species = models.ForeignKey(speciestype, on_delete=models.CASCADE) #foriegn key
     material_of_manufacture = models.ForeignKey(materialtype, on_delete=models.CASCADE) #foriegn key
     form_object_type = models.ForeignKey(formtype, on_delete=models.CASCADE) #foriegn key
-    quantity = models.IntegerField()
+    quantity = models.CharField(max_length=100) #changed
     measurement_diameter = models.FloatField()
     length = models.FloatField()
     width = models.FloatField()
-    height = models.FloatField()
-    measurement_notes = models.CharField(max_length=50)
+    height = models.CharField(max_length=50)
+    measurement_notes = models.CharField(max_length=750)
     weight = models.FloatField()
     weight_notes = models.CharField(max_length=50)
     sivilich_diameter = models.FloatField()
@@ -88,20 +88,20 @@ class your_table(models.Model):
     date_catalogued = models.DateTimeField()
     location_in_repository= models.CharField(max_length=50)
     platlot = models.CharField(max_length=50)
-    found_at_depth = models.FloatField()
-    longitude = models.FloatField()
-    latitude = models.FloatField()
+    found_at_depth = models.CharField(max_length=50) #changed
+    longitude = models.CharField(max_length=20) #changed
+    latitude = models.CharField(max_length=20) #changed
     distance_from_datum = models.CharField(max_length=25)
     found_in_grid = models.ForeignKey(gridnames, on_delete=models.CASCADE) #foriegn key
-    excavator = models.CharField(max_length=25)
+    excavator = models.CharField(max_length=50)
     notes = models.CharField(max_length=500)
     images = models.CharField(max_length=500)
-    data_double_checked_by = models.CharField(max_length=500)
-    qsconcerns = models.CharField(max_length=500)
-    druhlcheck = models.CharField(max_length=500)
-    sources_for_id = models.CharField(max_length=500)
-    location = models.CharField(max_length=90)
-    storage_location = models.CharField(max_length=90)
-    uhlflages = models.CharField(max_length=90)
+    data_double_checked_by = models.CharField(max_length=50)
+    qsconcerns = models.CharField(max_length=100)
+    druhlcheck = models.CharField(max_length=50)
+    sources_for_id = models.CharField(max_length=1000)
+    location = models.CharField(max_length=100)
+    storage_location = models.CharField(max_length=50)
+    uhlflages = models.CharField(max_length=250)
 
 

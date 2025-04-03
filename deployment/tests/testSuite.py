32ce5b532430dfa30deaefe6dafb74a3e20df0bb
Rory Paul <prory101@gmail.com>
2025-04-03 19:10:29 -0400

    Changing file paths

--- deployment/tests/testSuite.py
+++ deployment/tests/testSuite.py
@@ -2,20 +2,20 @@ import unittest
 import sys
 
 sys.path.append("myapp/tests")
-import test_create_user
-import test_artifact_model
-import test_change_password
-import test_resend_verification
+import app.myapp.tests.test_create_user
+import app.myapp.tests.test_artifact_model
+import app.myapp.tests.test_change_password
+import app.myapp.tests.test_resend_verification
 sys.path.append("..")
 
 
 sys.path.append("../../deployment/tests/UAT_tests")
 def load_tests(loader, standard_tests, pattern):
     test_suite = unittest.TestSuite()
-    test_suite.addTests(loader.loadTestsFromModule(test_create_user))
-    test_suite.addTests(loader.loadTestsFromModule(test_artifact_model))
-    test_suite.addTests(loader.loadTestsFromModule(test_change_password))
-    test_suite.addTests(loader.loadTestsFromModule(test_resend_verification))
+    test_suite.addTests(loader.loadTestsFromModule(app.myapp.tests.test_create_user))
+    test_suite.addTests(loader.loadTestsFromModule(app.myapp.tests.test_artifact_model))
+    test_suite.addTests(loader.loadTestsFromModule(app.myapp.tests.test_change_password))
+    test_suite.addTests(loader.loadTestsFromModule(app.myapp.tests.test_resend_verification))
 
     return test_suite
 

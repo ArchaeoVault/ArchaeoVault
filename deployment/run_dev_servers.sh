410e7b498a5eff454fcb5a014e3534c71e6ca35a
Rory Paul <prory101@gmail.com>
2025-04-06 18:53:20 -0400

    output to file

---
+++ deployment/run_dev_servers.sh
@@ -0,0 +1,18 @@
+ssh root@152.42.155.23 <<EOF
+cd /var/www/html/ArchaeoVault
+source /var/www/html/ArchaeoVault/myprojectenv/bin/activate
+pip install -r requirements.txt
+cd app
+chmod +x manage.py
+python manage.py runserver > /dev/null 2>&1 &
+deactivate
+
+cd /var/www/html/ArchaeoVault/app/frontend
+npm start > /dev/null 2>&1 &
+
+cd /var/www/html/ArchaeoVault
+source /var/www/html/ArchaeoVault/myprojectenv/bin/activate
+cd /var/www/html/ArchaeoVault/app
+python manage.py test ../deployment/tests > test_results.log 2>&1
+exit
+EOF

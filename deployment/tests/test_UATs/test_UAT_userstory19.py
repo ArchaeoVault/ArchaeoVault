181211c33184480fca6c6227e56063e41cfbb171
FrawstHawk <lorrock7@gmail.com>
2025-04-07 14:22:44 -0400

    adding localhost back

--- deployment/tests/test_UATs/test_UAT_userstory19.py
+++ deployment/tests/test_UATs/test_UAT_userstory19.py
@@ -52,7 +52,7 @@ class test_UAT_userstory19(unittest.TestCase):
 			self.driver = webdriver.Chrome(options=chrome_options)
 		else:
 			self.driver = webdriver.Chrome()
-		self.driver.get("https://www.python.org")
+		self.driver.get("http://localhost:3000")
 		login_page_button = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Login")))
 		login_page_button.click()
 

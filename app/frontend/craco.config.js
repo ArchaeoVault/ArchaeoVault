c67862c2badbd8913a9b8b70988fdc3e4ec7cf05
Rory Paul <prory101@gmail.com>
2025-04-07 18:44:13 -0400

    craco config

--- app/frontend/craco.config.js
+++ app/frontend/craco.config.js
@@ -1,5 +1,16 @@
 module.exports = {
     devServer: {
       allowedHosts: 'all',  // This allows all hosts
+      port: 3000, // Ensure it's using the expected port
+      proxy: {
+        '/api': 'http://localhost:8000',  // Ensure this is pointing to the right backend
+      }
     },
   };
+
+  module.exports = {
+    devServer: {
+      
+      
+    }
+  }

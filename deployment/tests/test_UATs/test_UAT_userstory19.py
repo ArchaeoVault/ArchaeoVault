ff18b5a11eabc2f58099c36b1fc48ba5a816286a
FrawstHawk <lorrock7@gmail.com>
2025-04-07 14:19:50 -0400

    user data dir change

--- deployment/tests/test_UATs/test_UAT_userstory19.py
+++ deployment/tests/test_UATs/test_UAT_userstory19.py
@@ -32,7 +32,7 @@ class test_UAT_userstory19(unittest.TestCase):
 			self.kill_chrome_processes()
 			chromedriver_autoinstaller.install()
 			chrome_options = webdriver.ChromeOptions()
-			raise ValueError('$$$$$$$$$$$' + chrome_options.user_data_dir)
+			# raise ValueError('$$$$$$$$$$$' + chrome_options.user_data_dir)
 			chrome_options.add_argument("--headless=new") # for Chrome >= 109
 			
 			chrome_options.add_argument("--no-sandbox")
@@ -40,8 +40,8 @@ class test_UAT_userstory19(unittest.TestCase):
 			chrome_options.add_argument("--disable-dev-shm-usage")
 			chrome_options.add_argument("--disable-gpu")
 			chrome_options.add_argument("--remote-debugging-port=9222")
-			#self.user_data_dir = tempfile.mkdtemp()
-			chrome_options.add_argument("--user-data-dir=/home/root/chrome-profiles/my-selenium-profile")
+			self.user_data_dir = tempfile.mkdtemp()
+			chrome_options.add_argument("--user-data-dir={self.user_data_dir}")
 
 			chrome_options.binary_location = "/usr/bin/google-chrome"
 			#CHROMEDRIVER_PATH = '/usr/bin/chromedriver'

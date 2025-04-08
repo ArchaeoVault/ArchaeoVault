65208f4363bbec041add6548a704a10fe39e6e8d
Rory Paul <prory101@gmail.com>
2025-04-08 19:42:38 -0400

    less time

--- deployment/tests/test_UATs/test_UAT_userstory19.py
+++ deployment/tests/test_UATs/test_UAT_userstory19.py
@@ -53,7 +53,7 @@ class test_UAT_userstory19(unittest.TestCase):
 			self.driver = webdriver.Chrome(options=chrome_options)
 		else:
 			self.driver = webdriver.Chrome()
-		time.sleep(1000)
+		time.sleep(10)
 		self.driver.get("http://152.42.155.23:3000/")
 		login_page_button = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Login")))
 		login_page_button.click()

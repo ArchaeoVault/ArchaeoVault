de315a8d2a83a76d324980a4c9157c0ddcf99eca
Rory Paul <prory101@gmail.com>
2025-04-23 16:25:45 -0400

    fix id field

--- app/myapp/models.py
+++ app/myapp/models.py
@@ -114,6 +114,6 @@ class your_table(models.Model):
 
 
 class imagetable(models.Model):
-    id = models.ForeignKey(your_table, on_delete=models.CASCADE) #foriegn key
+    your_table_id = models.ForeignKey(your_table, on_delete=models.CASCADE) #foriegn key
     filepath = models.CharField(max_length=100,db_column='filepath')
     

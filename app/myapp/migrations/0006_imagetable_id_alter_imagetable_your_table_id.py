5ed01fe574eaea8a844623caa756fea7a41013b2
Rory Paul <prory101@gmail.com>
2025-04-24 17:33:13 -0400

    removing default value of id

--- app/myapp/migrations/0006_imagetable_id_alter_imagetable_your_table_id.py
+++ app/myapp/migrations/0006_imagetable_id_alter_imagetable_your_table_id.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='imagetable',
             name='id',
-            field=models.BigAutoField(auto_created=True, default=0, primary_key=True, serialize=False, verbose_name='ID'),
+            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
             preserve_default=False,
         ),
         migrations.AlterField(

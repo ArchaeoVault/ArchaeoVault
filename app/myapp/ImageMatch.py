e7f9c67f61cbf20fc2e7b3c5c2eeecde1fb98a37
Rory Paul <prory101@gmail.com>
2025-04-24 09:23:21 -0400

    removing comments

--- app/myapp/ImageMatch.py
+++ app/myapp/ImageMatch.py
@@ -15,8 +15,7 @@ from myapp.models import your_table
 from myapp.models import imagetable
 #######################
 
-#dir_to_check =  r'/var/www/html/artifact_images/FINISHED JPG'
-dir_to_check = r'C:\Users\prory\OneDrive\Documents\2024-2025\Spring 2025\Capstone\image_test'
+dir_to_check =  r'/var/www/html/artifact_images/FINISHED JPG'
 
 #Get list of images in the chosen directory
 images = os.listdir(dir_to_check)
@@ -34,7 +33,6 @@ for image in images:
     catalog_number = catalog_number[:-1]
 
     # Get id by using select statement with catalog #
-    # select id from public.myapp_your_table where "Catalog # (day inventory)" = '2022-9-29-576'
     artifacts = your_table.objects.filter(catalog_number__icontains=catalog_number)
     for artifact in artifacts:
         artifact_id = artifact.id

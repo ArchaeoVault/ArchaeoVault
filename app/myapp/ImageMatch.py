0fdb961789e6f2d2d59b8436515bba2e0fa8bc4e
Rory Paul <prory101@gmail.com>
2025-04-25 18:08:41 -0400

    fixed issue with commas in filename

--- app/myapp/ImageMatch.py
+++ app/myapp/ImageMatch.py
@@ -16,8 +16,8 @@ from myapp.models import your_table
 from myapp.models import imagetable
 #######################
 
-dir_to_check =  r'/var/www/html/artifact_images/FINISHED JPG'
-
+# dir_to_check =  r'/var/www/html/artifact_images/FINISHED JPG'
+dir_to_check = r'C:\Users\prory\OneDrive\Documents\2024-2025\Spring 2025\Capstone\image_test'
 #Get list of images in the chosen directory
 images = os.listdir(dir_to_check)
 
@@ -26,20 +26,38 @@ for image in images:
 
     #Split each image by dash and if the substring is a number concatenate to extract only the catalog number from the file name
     split_img = image.split("-")
+    contains_comma = False
+    comma_substr = ""
+    catalog_numbers = []
+
     for substr in split_img:
         if substr.isnumeric():
             catalog_number += substr + "-"
-    
+        elif((substr.find(",") != -1)):
+            contains_comma = True
+            comma_substr = substr
+
     #Remove unecessary dash from end of catalog number
     catalog_number = catalog_number[:-1]
 
+    if contains_comma:
+        split_catalog = comma_substr.split(",")
+        for letter in split_catalog:
+            new_catalog_number = catalog_number + '-' + letter
+            catalog_numbers.append(new_catalog_number)
+    else:
+        
+        catalog_numbers.append(catalog_number)
+
+
     # Get id by using select statement with catalog #
-    artifacts = your_table.objects.filter(catalog_number__icontains=catalog_number)
-    for artifact in artifacts:
-        artifact_id = artifact.id
-        max_id = your_table.objects.aggregate(Max('id'))['id__max']
-        if max_id is None:
-            max_id = 0
-        imagetable.objects.create(your_table_id=artifact, filepath=image, id=max_id+1)
-        print("Artifact ID: ",artifact_id," Catalog Number: ",catalog_number," FileName: ",image, "\n")
+    for catalog_number in catalog_numbers:
+        artifacts = your_table.objects.filter(catalog_number=catalog_number)
+        for artifact in artifacts:
+            artifact_id = artifact.id
+            max_id = your_table.objects.aggregate(Max('id'))['id__max']
+            if max_id is None:
+                max_id = 0
+            #imagetable.objects.create(your_table_id=artifact, filepath=image, id=max_id+1)
+            print("Artifact ID: ",artifact_id," Catalog Number: ",catalog_number," FileName: ",image, "\n")
 

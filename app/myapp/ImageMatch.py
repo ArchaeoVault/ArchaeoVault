dbe1718e9b443d4c80c8899aa917187de97a628c
Rory Paul <prory101@gmail.com>
2025-04-26 10:28:08 -0400

    updating to work with new file name formats

--- app/myapp/ImageMatch.py
+++ app/myapp/ImageMatch.py
@@ -2,12 +2,18 @@ import os
 import sys
 import django
 from django.db.models import Max
+
 #Setup django environment
 # Add the project root to Python's path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # Going up two levels
 
 # Set the Django settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.local')  # replace with your actual project name
+env = os.environ.get('DJANGO_ENV', 'None')
+if env == 'production':
+    settings_dir = 'app.production'
+else:
+    settings_dir = 'app.local'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_dir)  # replace with your actual project name
 
 # Initialize Django
 django.setup()
@@ -16,8 +22,8 @@ from myapp.models import your_table
 from myapp.models import imagetable
 #######################
 
-# dir_to_check =  r'/var/www/html/artifact_images/FINISHED JPG'
-dir_to_check = r'C:\Users\prory\OneDrive\Documents\2024-2025\Spring 2025\Capstone\image_test'
+dir_to_check =  r'/var/www/html/artifact_images/FINISHED JPG'
+
 #Get list of images in the chosen directory
 images = os.listdir(dir_to_check)
 
@@ -25,7 +31,12 @@ for image in images:
     catalog_number = ""
 
     #Split each image by dash and if the substring is a number concatenate to extract only the catalog number from the file name
-    split_img = image.split("-")
+
+    #Some file names have only the catalog number so when this is the case removing the jpeg allows us to extract the whole number
+    image_cleaned = image.replace(".jpeg", "")
+    
+    #Accounting for some files have catalogue number of 'MD'
+    split_img = image_cleaned.split("-")
     contains_comma = False
     comma_substr = ""
     catalog_numbers = []
@@ -36,10 +47,11 @@ for image in images:
         elif((substr.find(",") != -1)):
             contains_comma = True
             comma_substr = substr
+        elif('MD' in substr):
+            catalog_number += substr + '-'
 
     #Remove unecessary dash from end of catalog number
     catalog_number = catalog_number[:-1]
-
     if contains_comma:
         split_catalog = comma_substr.split(",")
         for letter in split_catalog:
@@ -55,9 +67,15 @@ for image in images:
         artifacts = your_table.objects.filter(catalog_number=catalog_number)
         for artifact in artifacts:
             artifact_id = artifact.id
-            max_id = your_table.objects.aggregate(Max('id'))['id__max']
+            max_id = imagetable.objects.aggregate(Max('id'))['id__max']
             if max_id is None:
                 max_id = 0
-            #imagetable.objects.create(your_table_id=artifact, filepath=image, id=max_id+1)
-            print("Artifact ID: ",artifact_id," Catalog Number: ",catalog_number," FileName: ",image, "\n")
+            
+            existing_record = imagetable.objects.filter(filepath=image,your_table_id=artifact.id)
+            if(len(existing_record)==0):
+                imagetable.objects.create(your_table_id=artifact, filepath=image, id=max_id+1)
+                print("Artifact ID: ",artifact_id," Catalog Number: ",catalog_number," FileName: ",image, "\n")
+            else:
+                print("Already exists ","Artifact ID: ",artifact_id," Catalog Number: ",catalog_number," FileName: ",image, "\n")
+            
 
